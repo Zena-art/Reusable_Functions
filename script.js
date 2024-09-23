ece25cc44f4f8fd8b06bdd6462e07274d17a1e5e
Zena-art <zena.nazim@gmail.com>
2024-09-23 18:39:28 -0500

    Second exercise is working

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
+//part 1
+
+
 // Creating array
 let arr = [4, 8, 7, 13, 12];
 
 // Function to find the sum of the array using recursion
 function sumArray(arr, index) {
     if (index === arr.length) {
-        return 0;
+        return 0; // if empty array 
     }
     return arr[index] + sumArray(arr, index + 1);
 }
 
 console.log("Sum is " + sumArray(arr, 0));
+
+
+// part 2 
+
+
+function calculateAvg(arr){
+  let sum = arr.reduce((acc, curr) => acc + curr, 0); //Summing all the elements
+  let average = sum / arr.length;
+  return average;
+}
+
+let numbers = [10, 20, 30];
+console.log(calculateAvg(numbers));
