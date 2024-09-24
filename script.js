7a958b0dd94aecf48400bfd059d58e5a4265dfe8
Zena-art <zena.nazim@gmail.com>
2024-09-24 09:42:48 -0500

    part 2 sort array is done

--- script.js
+++ script.js
@@ -80,3 +80,49 @@ function printNumbers(n){
   console.log(n);
 }
 printNumbers(9);
+
+
+// Part 2; Thinking Methodically
+
+/** 
+ * When functions are built into objects, like Arrays, they are referred to as “methods” of those objects. Many methods, including Array methods, require “callback functions” to determine their behavior.
+For the tasks below, use the following data to test your work:
+[{ id: "42", name: "Bruce", occupation: "Knight", age: "41" },
+ { id: "48", name: "Barry", occupation: "Runner", age: "25" },
+ { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },
+ { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },
+ { id: "7", name: "Bilbo", occupation: "None", age: "111" }]
+Use callback functions alongside Array methods to accomplish the following:
+Sort the array by age.
+Filter the array to remove entries with an age greater than 50.
+Map the array to change the “occupation” key to “job” and increment every age by 1.
+Use the reduce method to calculate the sum of the ages.
+Then use the result to calculate the average age. 
+ */
+
+
+let userData = [
+  { id: "42",
+    name: "Bruce", 
+    occupation: "Knight", 
+    age: "41" },
+  { id: "48", 
+    name: "Barry", 
+    occupation: "Runner", 
+    age: "25" },
+  { id: "57", 
+    name: "Bob", 
+    occupation: "Fry Cook", 
+    age: "19" },
+  { id: "63", 
+    name: "Blaine", 
+    occupation: "Quiz Master", 
+    age: "58" },
+  { id: "7", 
+    name: "Bilbo", 
+    occupation: "None", 
+    age: "111" }
+];
+ 
+userData.sort((a, b)=> a.age - b.age);
+console.log(userData);
